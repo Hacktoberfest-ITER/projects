3ff99e2f6de8f710c85e3eaccfa8e3aa0dcd8191
Satyabrata Mishra <mishrasatyabrata27@gmail.com>
2021-10-12 22:49:45 +0530

    Made Some Changes In Navbar

--- travelwebsite/react/src/App.js
+++ travelwebsite/react/src/App.js
@@ -3,15 +3,21 @@ import Footer from './Components/Footer';
 import Navbar from './Components/Navbar'
 import Openingpage from './Components/Openingpage';
 import Openingpage2 from './Components/Openingpage2';
-// import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import Service from './Components/Service'
+import Contact from './Components/Contact'
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 function App() {
   return (
-     <div className="app">
-       <Navbar/>
-       <Openingpage/>
-       <Openingpage2/>
-       <Footer/>
-     </div>
+       <Router>
+          <Navbar/>
+          <Openingpage/>
+          <Openingpage2/>
+          <Switch>
+            <Route path="/Service.js"><Service/></Route>
+            <Route path="/Contact.js"><Contact/></Route>
+          </Switch>
+          <Footer/>
+     </Router>
   );
 }
 export default App;
---
+++ travelwebsite/react/src/Components/Service.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Service() {
+    return (
+        <div>
+            Add Your File For Service Here.
+        </div>
+    )
+}
